33740e811d7e29987611e2c0a6f88dd86992ca44
Ashutosh Sudhir Kumbhar <ashutoshkumbhar@Ashutoshs-MacBook-Air.local>
2023-02-17 09:40:25 +0530

    done

--- src/inject/dependancy.js
+++ src/inject/dependancy.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const dependancyInjector = function DependancyInjector(app){
     app.use('/bootstrap/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
-    app.use('/bootstrap/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+    app.use('/bootstrap/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
 }
 
 module.exports = dependancyInjector;
